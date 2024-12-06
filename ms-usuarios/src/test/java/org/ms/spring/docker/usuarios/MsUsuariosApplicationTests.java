f4863c7bf32b85e535cc79c46865ea956c0313b3
SKOTOS99 <felipd95@hotmail.com>
2024-12-06 09:46:12 -0600

    init commit

---
+++ ms-usuarios/src/test/java/org/ms/spring/docker/usuarios/MsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ms.spring.docker.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
