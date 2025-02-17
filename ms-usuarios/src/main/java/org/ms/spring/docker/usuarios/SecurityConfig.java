51645b6f0c5ebc5a3c4d15b5a5d3e4612836f35d
SKOTOS99 <felipd95@hotmail.com>
2025-02-17 12:04:18 -0600

    implement resource server

--- ms-usuarios/src/main/java/org/ms/spring/docker/usuarios/SecurityConfig.java
+++ ms-usuarios/src/main/java/org/ms/spring/docker/usuarios/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.ms.spring.docker.usuarios;
 
+import okhttp3.internal.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,11 +13,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().authenticated()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().oauth2Login(oauth2Login-> oauth2Login.loginPage("oauth2/authorization/ms-usuarios-client"))
-                .oauth2Client(Customizer.withDefaults());
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/listar", "/listar/id/{id}").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+                        .requestMatchers(HttpMethod.POST, "/guardar/usuario").hasAuthority("SCOPE_write")
+                        .requestMatchers(HttpMethod.PUT, "/actualizar/usuario/{id}").hasAuthority("SCOPE_write")
+                        .requestMatchers(HttpMethod.DELETE, "/eliminar/usuario/{id}").hasAuthority("SCOPE_write")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(oauth2 -> oauth2.loginPage("oauth2/authorization/ms-usuarios-client"))
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
         return http.build();
     }
 }
